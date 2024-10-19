8385abd96b64a39f7e6ebae65ffdb19d495c54bf
Muhamad Zainul Kamal <zainul.intern.pasti@pegadaian.co.id>
2024-10-19 21:38:09 +0700

    Variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    variable();
+}
+
+fn variable() {
+    // dafault variable in rust is immutable that means it can't be changed
+    let name = "Zainul";
+    // name = "Ajay"; ERROR
+    println!("Hello, {}!", name);
+
+    // mut keywoard in rust is mutable that means it can be changed
+    let mut name2 = "Zainul";
+    name2 = "Ajay";
+    println!("Hello, {}!", name2);
 }

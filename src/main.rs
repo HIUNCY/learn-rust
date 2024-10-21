4538532dd684b6a72818909c590d0ba43c5b5324
Muhamad Zainul Kamal <zainul.intern.pasti@pegadaian.co.id>
2024-10-21 09:48:31 +0700

    Numeric Operator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     variable();
+    numeric_operator();
 }
 
 fn variable() {
@@ -12,4 +13,19 @@ fn variable() {
     let mut name2 = "Zainul";
     name2 = "Ajay";
     println!("Hello, {}!", name2);
+}
+
+fn numeric_operator() {
+    let a = 10;
+    let b = 20;
+    let c = a + b;
+    println!("The sum of {} and {} is {}", a, b, c);
+    let d = a - b;
+    println!("The difference of {} and {} is {}", a, b, d);
+    let e = a * b;
+    println!("The product of {} and {} is {}", a, b, e);
+    let f = a / b;
+    println!("The quotient of {} and {} is {}", a, b, f);
+    let g = a % b;
+    println!("The remainder of {} and {} is {}", a, b, g);
 }
